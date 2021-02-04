da584212fbfb3616fd1c16da5c715aca99f9bfc0
malekmahjoub635 <malouka99@live.com>
2021-02-04 13:14:37 +0000

    adding files

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,11 +12,11 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 a = n % 10;
-if (n > 5)
-printf("%d and is greater than 5\n", n);
-else if (n < 5)
-printf("%d and is less than 6 and not 0\n", n);
+if (a > 5)
+printf("%d and is greater than 5\n", n, a);
+else if (a < 5)
+printf("%d and is less than 6 and not 0\n", n, a);
 else
-printf("%d and is 0\n", n);
+printf("%d and is 0\n", n, a);
 return (0);
 }
