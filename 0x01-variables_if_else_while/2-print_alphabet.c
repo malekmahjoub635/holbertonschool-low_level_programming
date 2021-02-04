fb862064a5b6aba251b9d8917154ebd73bc6fdab
malekmahjoub635 <malouka99@live.com>
2021-02-04 14:11:33 +0000

    adding files

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+char 
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+return (0);
+}
