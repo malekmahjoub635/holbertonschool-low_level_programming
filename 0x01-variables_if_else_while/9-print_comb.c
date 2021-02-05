1f075e6eeef01013566a5b1a30693f20a75eeed5
malekmahjoub635 <malouka99@live.com>
2021-02-05 16:11:51 +0000

    adding files

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,9 +9,14 @@ int main(void)
 {
 int n;
 for (n = 0; n <= 9; n++)
-putchar(n);
-if (n < 9)
+  {
+putchar('0' + n);
+if (n != 9)
+{
 putchar(',');
 putchar(' ');
+}
+}
+ putchar('\n');
 return (0);
 }
