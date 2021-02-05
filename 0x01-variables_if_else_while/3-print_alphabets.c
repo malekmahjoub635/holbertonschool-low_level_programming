8b4b48382a8da32349bfcda6153796a1db388bb9
malekmahjoub635 <malouka99@live.com>
2021-02-05 11:41:10 +0000

    adding files

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+putchar ('\n');
+for (ch = 'A'; ch <= 'Z'; ch++)
+putchar(ch);
+putchar ('\n');
+return (0);
+}
