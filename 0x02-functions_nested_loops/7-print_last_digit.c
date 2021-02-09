a9c5931d4737833b3784801d841bc596ef9ade73
malekmahjoub635 <malouka99@live.com>
2021-02-09 14:05:53 +0000

    adding files

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,16 +2,14 @@
 
 /**
  * main - check the code for Holberton School students.
- *
+ *@n: is for parameter
  * Return: Always 0.
  */
 int print_last_digit(int n);
 {
-int i;
+int n, i;
 i = n % 10;
 if (i < 0)
-{
 _putchar(i + '0');
-}
 return (i);
 }
