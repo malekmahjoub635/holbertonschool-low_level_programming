51abf494bac19aeb4c7fe7144638b156e0d01920
malekmahjoub635 <malouka99@live.com>
2021-02-09 12:54:16 +0000

    adding files

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _abs - returns the absolue value of n
+ * @r: show the value
+ * Return: r or -r  success
+ */
+int _abs(int i)
+{
+if (i >= 0)
+return (i);
+else
+return (-i);
+}
