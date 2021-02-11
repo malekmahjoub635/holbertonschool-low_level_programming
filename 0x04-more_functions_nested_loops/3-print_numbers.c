8273189da6550526259c3791d916363918d17373
malekmahjoub635 <malouka99@live.com>
2021-02-11 13:54:00 +0000

    adding files

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *print_numbers - a function that prints the numbers
+ * Return: 0
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+_putchar(i + '0');
+_putchar('\n');
+}
