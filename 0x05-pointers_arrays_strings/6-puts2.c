cdae98568ca116611eee0d6aa90085e60c032cb3
malekmahjoub635 <malouka99@live.com>
2021-02-16 11:09:42 +0000

    adding files

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "holberton.h"
 /**
  *puts2 -  a function that prints every other character of a string
@@ -7,6 +8,7 @@
 void puts2(char *str)
 {
 int i;
+i = 0;
 for (i = 0; str[i] != '\0'; i = i + 2)
 {
 _putchar(str[i]);
