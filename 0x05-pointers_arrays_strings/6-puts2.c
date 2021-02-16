b876cb798c6386d22713c6e7dd67677fc386a4dc
malekmahjoub635 <malouka99@live.com>
2021-02-16 14:47:45 +0000

    adding files

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,12 @@
  */
 void puts2(char *str)
 {
-  int i, y;
-
+int i, y;
 for (i = 0; str[i] != '\0'; i++)
 {
 }
- i = i - 1;
- for (y = 0; y <= i ; y=y+2)
-   _putchar(str[y]);
- _putchar('\n');
+i = i - 1;
+for (y = 0; y <= i ; y=y+2)
+_putchar(str[y]);
+_putchar('\n');
 }
