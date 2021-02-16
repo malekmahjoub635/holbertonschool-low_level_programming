fcec725beb797a7d3d46277dc79190052b9370e9
malekmahjoub635 <malouka99@live.com>
2021-02-16 07:36:47 +0000

    adding files

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- *
+ *print_rev - a function that prints a string, in reverse
  *Return: 0
  */
 void print_rev(char *s)
@@ -8,8 +8,9 @@ void print_rev(char *s)
 int n = 0;
 while (str[n] != '\0')
 {
-n--;
+print_rev(s);
 _putchar(str[n]);
 }
+n++;
 _putchar('\n');
 }
