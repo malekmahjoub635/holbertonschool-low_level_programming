fe1479d1a3567bac6049872a3a090a65b9a7f996
malekmahjoub635 <malouka99@live.com>
2021-02-16 11:04:05 +0000

    adding files

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,7 +6,7 @@
 void rev_string(char *s)
 {
 int i;
-for (i = 0; s [i] !='\0'; i--)
+for (i = 0; s [i] !='\0'; i++)
 {
 _putchar(s [i]);
 }
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 /**
  *puts2 -  a function that prints every other character of a string
- *
+ *@str: stfu
  *Return: 0
  */
 void puts2(char *str)
 {
 int i;
 i = 0;
-for (i = 0; str [i] !='\0'; i = i+2)
+for (i = 0; str[i] != '\0'; i = i + 2)
 {
-_putchar(str [i]);
+_putchar(str[i]);
 }
 _putchar('\n');
 }
