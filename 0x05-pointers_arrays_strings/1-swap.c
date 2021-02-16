2035f1fdc4a2d7c32ad34c69787fbe801e9cca1f
malekmahjoub635 <malouka99@live.com>
2021-02-16 07:19:17 +0000

    fixing betty

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,7 +2,7 @@
 /**
  *swap_int -  a function that swaps the values of two integers
  *@a: first parameter
- *@b: second parameter 
+ *@b: second parameter
  *Return: 0
  */
 void swap_int(int *a, int *b)
