9d93cad36d6acf7415cd95ac6d757a0ae8ed6a86
malekmahjoub635 <malouka99@live.com>
2021-02-16 10:57:47 +0000

    adding files

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,13 +1,12 @@
 #include "holberton.h"
 /**
  *rev_string -  a function that reverses a string
- *
  *Return: 0
  */
 void rev_string(char *s)
 {
 int i;
-for (i = 0; s [i] !='\0'; i++)
+for (i = 0; s [i] !='\0'; i--)
 {
 _putchar(s [i]);
 }
