93db9bc87818381bfcfe15d15b950f73bb62379f
malekmahjoub635 <malouka99@live.com>
2021-02-16 12:02:25 +0000

    adding files

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *puts_half - a function that prints half of a string
+ *
+ *Return: 0
+ */
+void puts_half(char *str)
+{
+
+
+}
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *print_array - a function that prints n elements of an array of integers
+ *
+ *Return: 0
+ */
+void print_array(int *a, int n)
+{
+  int n [5] = {98,402,-189,298,-1024};
+  int y [5];
+  printf("%d ", n [n]);
+}
