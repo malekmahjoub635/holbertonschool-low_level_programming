e45bee91650ed35201e78226d0e69e44115edb1e
malekmahjoub635 <malouka99@live.com>
2021-02-18 13:56:16 +0000

    adding files

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *_strcat -  a function that concatenates two strings. 
+ *@src: strinng
+ *@dest: another string
+ *Return: 0
+ */
+char *_strcat(char *dest, char *src)
+{
+int i, j;
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+j = 0;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
+}
