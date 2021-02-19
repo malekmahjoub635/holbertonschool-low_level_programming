410e03d92509aa4257f673677b5f484c1e7b5683
malekmahjoub635 <malouka99@live.com>
2021-02-19 14:43:32 +0000

    adding files

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,10 +1,24 @@
 #include "holberton.h"
 /**
- *rot13 - a function that encodes a string using rot13
- * Return: 0
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
+ * Return: s
  */
-char *rot13(char *)
+char *rot13(char *s)
 {
-
-
+ int i, j;
+char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; a[j] != '\0'; j++)
+{
+if (s[i] == a[j])
+{
+s[i] = b[j];
+break;
+}
+}
+}
+return (s);
 }
