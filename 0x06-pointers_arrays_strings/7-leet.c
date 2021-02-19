3d0425253305d4c7afb1f87a3ca0080c0649aa0f
malekmahjoub635 <malouka99@live.com>
2021-02-19 13:54:53 +0000

    adding files

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *leet -  a function that encodes a string into 1337
+ * 
+ *Return: 0
+ */
+char *leet(char *s)
+{
+int i, j;
+char *c = "aAeEoOtTlL";
+char *l = "43071";
+for (i = 0; s[i] != '\0'; i ++)
+{
+for (j = 0; j < 10; j++)
+{
+if (s[i] == c[j])
+{
+s[i] = l[j];
+}
+}
+}
+return (s);
+}
