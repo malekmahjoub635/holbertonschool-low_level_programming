4cd464013c430d13d245e81354615b1c93f63897
malekmahjoub635 <malouka99@live.com>
2021-02-19 08:46:54 +0000

    adding files

---
+++ 0x06-pointers_arrays_strings/.#2-strncpy.c
@@ -0,0 +1,1 @@
+vagrant@vagrant-ubuntu-trusty-64.1507:1613723253
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,10 @@
 #include "holberton.h"
 /**
- *_strcat -  a function that concatenates two strings. 
- *@src: string
- *@dest: another string
+ *_strncat -  a function that concatenates two strings
+ *
  *Return: 0
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 int i, j;
 i = 0;
@@ -14,7 +13,7 @@ while (dest[i] != '\0')
 i++;
 }
 j = 0;
-while (src[j] != '\0')
+while (j < n)
 {
 dest[i] = src[j];
 i++;
