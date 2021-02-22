6ae33ba73ddbadbc856845769339629475c40da4
malekmahjoub635 <malouka99@live.com>
2021-02-22 11:08:43 +0000

    adding files

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,17 +1,22 @@
 #include "holberton.h"
 /**
- *memcpy - a function that copies memory area
+ *_memcpy - a function that copies memory area
  *@dest: memory area
  *@src: another memory area
+ *@n: num of times
  *Return: 0
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i;
-
-
-
-
+unsigned int j;
+for (j = 0; j < n && src[j] != '\0'; j++)
+{
+dest[j] = src[j];
+}
+while (j < n)
+{
+dest[j] = '\0';
+j++;
 }
 return (dest);
 }
