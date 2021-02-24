4c68fa7d7e64b782e1382e41202b65709815deca
malekmahjoub635 <malouka99@live.com>
2021-02-24 18:01:42 +0000

    fixing betty

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,46 +1,43 @@
 #include "holberton.h"
-int _strlen_recursion(char *s);
-int pal(char *s, int len, int i);
 /**
- *is_palindrome - function
- *@s: PAL.
- * Return: 0
- */
-int is_palindrome(char *s)
-{
-int len = _strlen_recursion(s);
-return (pal(s, len - 1, 0));
-}
-/**
- *_strlen_recursion - check the code
- *@s: pointer to char
- *Return: 0
+ * _strlen_recursion - length
+ *@s: string
+ *Return: length of a string
  */
 int _strlen_recursion(char *s)
 {
-if (*s != '\0')
+if (*s == '\0')
 {
-return (_strlen_recursion(s + 1) + 1);
+return (0);
 }
+s++;
+return (1 + _strlen_recursion(s));
+}
+/**
+ * palindrome - to verify palindrome
+ *@s: a string
+ *@len: the length
+ *Return: the value
+ */
+int palindrome(char *s, int len)
+{
+if (len <= 1)
+return (1);
+else if (*s == *(s + len - 1))
+return (palindrome(s + 1, len - 2));
+else
 return (0);
 }
 /**
- *pal - is it
- *@s: char
- *@len: length
- *@i: account
- *Return: 0
+ * is_palindrome - verify if is palindrome
+ *@s: a string
+ *Return: 1 if palindrome else 0
  */
-int pal(char *s, int len, int i)
+int is_palindrome(char *s)
 {
-if (s[i] == s[len - i] && i == len / 2)
-{   
+int len;
+len = _strlen_recursion(s);
+if (len <= 1)
 return (1);
-}
-else if (s[i] == s[len - i])
-{
-return (pal(s, len, i + 1));
-}
-else
-return (0);
+return (palindrome(s, len));
 }
