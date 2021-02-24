32c7445cc42d8418871c95cbe43995af3ac08a7f
malekmahjoub635 <malouka99@live.com>
2021-02-24 18:04:59 +0000

    fixing betty

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,43 +1,30 @@
+#include <stdio.h>
 #include "holberton.h"
 /**
- * _strlen_recursion - length
- *@s: string
- *Return: length of a string
+ * _sqrt - Tests a
+ * @a: The number to test with b
+ * @b: The squared number to find the square
+ *
+ * Return: -1 else a
  */
-int _strlen_recursion(char *s)
+int _sqrt(int a, int b)
 {
-if (*s == '\0')
-{
-return (0);
-}
-s++;
-return (1 + _strlen_recursion(s));
-}
-/**
- * palindrome - to verify palindrome
- *@s: a string
- *@len: the length
- *Return: the value
- */
-int palindrome(char *s, int len)
-{
-if (len <= 1)
-return (1);
-else if (*s == *(s + len - 1))
-return (palindrome(s + 1, len - 2));
+if (a > b)
+return (-1);
+if ((a * a) == b)
+return (a);
 else
-return (0);
+return (_sqrt(++a, b));
 }
 /**
- * is_palindrome - verify if is palindrome
- *@s: a string
- *Return: 1 if palindrome else 0
+ * _sqrt_recursion - natural square root
+ * @n: The number
+ *
+ * Return: -1 and else the square root of n
  */
-int is_palindrome(char *s)
+int _sqrt_recursion(int n)
 {
-int len;
-len = _strlen_recursion(s);
-if (len <= 1)
-return (1);
-return (palindrome(s, len));
+if (n == 0)
+return (-1);
+return (_sqrt(1, n));
 }
