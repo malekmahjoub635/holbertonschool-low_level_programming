1b403910d49d5f0be61f55fa53c264eb12e5f27a
malekmahjoub635 <malouka99@live.com>
2021-03-09 08:50:34 +0000

    fix

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,4 +1,6 @@
-#include "1-pi.h"
-#include "1-pi.h"
+#ifndef "PI_H"
+#define "PI_H"
+
 #define PI 3.14159265359
+
 #endif
--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 /**
  * main - Prints the name of the file
