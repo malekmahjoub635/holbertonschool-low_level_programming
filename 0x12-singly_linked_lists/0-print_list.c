35c58ea07c17d68f6fb0c5cc2225fdbd4af51bf5
malekmahjoub635 <malouka99@live.com>
2021-03-24 09:34:09 +0000

    fix betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 /**
  *print_list - a function that prints all the elements of a list
  *@h: head
@@ -6,16 +7,15 @@
  */
 size_t print_list(const list_t *h)
 {
-int i;
-if (h == NULL)
-return (0);
-for (i = 0; h != NULL; i++)
+size_t i = 0;
+while (h)
 {
-if (h->str != NULL)
-printf("[%d] %s\n", h->len, h->str);
-else
+if (!h->str)
 printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
 h = h->next;
+i++;
 }
 return (i);
 }
