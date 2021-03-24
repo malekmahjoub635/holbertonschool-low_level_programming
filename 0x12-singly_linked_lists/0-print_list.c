0337e45bd3450a9caad83d8cc8a3354b330a7fb2
malekmahjoub635 <malouka99@live.com>
2021-03-24 09:41:53 +0000

    fix betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,4 @@
-#include "lists.h"
+B#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,7 +15,7 @@ while (h != NULL)
 if (h->str == NULL)
 printf("[0] (nil)\n");
 else
-printf("[%d] %s\n", h->len, h->str);
+printf("[%d] %s\n", h->i, h->str);
 h = h->next;
 i++;
 }
