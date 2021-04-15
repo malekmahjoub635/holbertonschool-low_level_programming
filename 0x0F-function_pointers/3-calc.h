f52a4feaaa3a41d296cd66dc4df632286224e5a2
malekmahjoub635 <malouka99@live.com>
2021-04-15 12:18:29 +0000

    Function pointers

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,4 +1,4 @@
-#ifndef CALC_H
+B#ifndef CALC_H
 #define CALC_h
 
 #include <stdio.h>
@@ -14,7 +14,7 @@ typedef struct op
 {
 char *op;
 int (*f)(int a, int b);
-} op_t,;
+} op_t;
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
