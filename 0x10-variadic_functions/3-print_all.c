e22c55585955dc849b7e409b5f31c140817d1daf
malekmahjoub635 <malouka99@live.com>
2021-04-15 14:44:31 +0000

    Variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,89 +1,46 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
-
 /**
- * printChar - prints a char for args
- * @ap: the arg
- * Return: void
- */
-void printChar(va_list ap)
-{
-printf("%c", va_arg(ap, int));
-}
-
-/**
- * printInt - prints an int for args
- * @ap: the arg
- * Return: void
- */
-void printInt(va_list ap)
-{
-printf("%d", va_arg(ap, int));
-}
-
-/**
- * printFloat - prints a float for args
- * @ap: the arg
- * Return: void
- */
-void printFloat(va_list ap)
-{
-printf("%f", va_arg(ap, double));
-}
-
-/**
- * printString - prints a string for args
- * @ap: the arg
- * Return: void
- */
-void printString(va_list ap)
-{
-char *ch;
-ch = va_arg(ap, char *);
-if (ch == NULL)
-{
-printf("(nil)");
-return;
-}
-printf("%s", ch);
-}
-/**
- * print_all - prints all arguments
- * @format: the format
- * Return: void
+ *print_all - print everything
+ *@format: format of arg
+ *
+ * Return: successful.
  */
 
 void print_all(const char * const format, ...)
 {
-int i, j;
-char *sep = "";
-va_list obj;
-form func[] = {
-{"c", printChar},
-{"i", printInt},
-{"f", printFloat},
-{"s", printString},
-{NULL, NULL},
-};
-va_start(obj, format);
-i = 0;
+unsigned int i = 0;
+char c, *ch, *separator = "";
+va_list ap;
+va_start(ap, format);
 while (format && format[i])
 {
-j = 0;
-while (func[j].c)
+c = format[i];
+switch (c)
 {
-if (*(func[j].c) == format[i])
-{
-printf("%s", sep);
-func[j].printTypef(obj);
-sep = ", ";
-}
-j++;
+case 'c':
+printf("%s%c", separator, va_arg(ap, int));
+separator = ", ";
+break;
+case 'i':
+printf("%s%d", separator, va_arg(ap, int));
+separator = ", ";
+break;
+case 'f':
+printf("%s%f", separator, va_arg(ap, double));
+separator = ", ";
+break;
+case 's':
+ch = va_arg(ap, char *);
+if (!ch)
+ch = "(nil)";
+printf("%s%s", separator, ch);
+separator = ", ";
+break;
 }
 i++;
 }
-va_end(obj);
 printf("\n");
+va_end(ap);
 }
