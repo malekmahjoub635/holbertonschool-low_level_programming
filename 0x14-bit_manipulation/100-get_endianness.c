c796959b3f9f91e4589e1ea329af64f067944fd3
malekmahjoub635 <malouka99@live.com>
2021-04-18 14:55:50 +0000

    bit manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * get_endianness - a function that checks endianness
+ * Return: 0 if big endian or 1 if little endian
+ */
+int get_endianness(void)
+{
+int i = 1;
+if (*(char *)&i == 1)
+return (1);
+else
+return (0);
+}
