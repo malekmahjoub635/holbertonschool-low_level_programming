d08a162a970bb03aa315be6d6e496c5c5dc986b5
malekmahjoub635 <malouka99@live.com>
2022-01-05 10:59:25 +0000

    fix

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,5 +8,8 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-return (hash_djb2(key) % size);
+unsigned long int hash, index;
+hash = hash_djb2(key);
+index = hash % size;
+return (index);
 }
