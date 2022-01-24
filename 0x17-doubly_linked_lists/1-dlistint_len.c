9d5f16129fce3cd22c1cd9953cd430267183cd75
malekmahjoub635 <malouka99@live.com>
2022-01-24 13:14:39 +0000

    fix

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -6,11 +6,11 @@
  */
 size_t dlistint_len(const dlistint_t *h);
 {
-int i;
-
-if (h == NULL)
-return (0);
-for (i = 0; h != NULL; i++)
+size_t len = 0;
+while (h)
+{
 h = h->next;
-return (i);
+len++;
+}
+return (len);
 }
