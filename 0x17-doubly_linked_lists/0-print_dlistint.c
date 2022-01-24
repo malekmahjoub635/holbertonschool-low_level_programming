c9bc003cb5aef506619174a8bf449e42cf4af028
malekmahjoub635 <malouka99@live.com>
2022-01-24 13:13:05 +0000

    fix

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,12 +9,12 @@
 size_t print_dlistint(const dlistint_t *h)
 
 {
-  int i;
-
-  for (i = 0; h != NULL; i++)
-    {
-      printf("%d\n", h->n);
-      h = h->next;
-    }
-  return (i);
+size_t len = 0;
+while (h)
+{
+printf("%d\n", h->n);
+h = h->next;
+len++;
+}
+return (len);
 }
