ce52124958dc1b4e17f7da798f6f79520506ac4d
malekmahjoub635 <malouka99@live.com>
2022-01-24 13:21:50 +0000

    fix

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -6,11 +6,8 @@
  */
 int sum_dlistint(dlistint_t *head)
 {
-int sum;
-
-if (head == NULL)
-return (0);
-for (sum = 0; head != NULL; sum++)
+int sum = 0;
+while (head)
 {
 sum += head->n;
 head = head->next;
